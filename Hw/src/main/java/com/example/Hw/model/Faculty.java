22bca0763ce5bb1a3aeaa37ba9bd76cd2462c0d4
AleksSkyr <greshnik1499@gmail.com>
2023-10-22 15:24:02 +0300

    d

--- Hw/src/main/java/com/example/Hw/model/Faculty.java
+++ Hw/src/main/java/com/example/Hw/model/Faculty.java
@@ -16,11 +16,18 @@ public class Faculty {
     private String name;
     private String color;
 
+//    public Faculty() {
+//    }
+//
+//    public Faculty(Long id, String name, String color) {
+//        this.id = id;
+//        this.name = name;
+//        this.color = color;
+//    }
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
@@ -28,7 +35,6 @@ public class Faculty {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -36,19 +42,6 @@ public class Faculty {
     public String getColor() {
         return color;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Faculty faculty)) return false;
-        return Objects.equals(getId(), faculty.getId()) && Objects.equals(getName(), faculty.getName()) && Objects.equals(getColor(), faculty.getColor());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), getColor());
-    }
-
     public void setColor(String color) {
         this.color = color;
     }
@@ -61,5 +54,4 @@ public class Faculty {
                 ", color='" + color + '\'' +
                 '}';
     }
-
 }
--- Hw/src/main/java/com/example/Hw/model/Student.java
+++ Hw/src/main/java/com/example/Hw/model/Student.java
@@ -5,11 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import java.util.Objects;
-
 @Entity
 public class Student {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,11 +15,14 @@ public class Student {
 
     public Student() {
     }
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
@@ -30,31 +30,15 @@ public class Student {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Student student)) return false;
-        return getAge() == student.getAge() && Objects.equals(getId(), student.getId()) && Objects.equals(getName(), student.getName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), getAge());
-    }
-
     @Override
     public String toString() {
         return "Student{" +
